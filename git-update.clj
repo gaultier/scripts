433b54bb841873c025bc203ed0aacaa551d7a6b1
Philippe Gaultier <philippe.gaultier@ppro.com>
2022-04-04 16:40:01 +0200

    use pmap

--- git-update.clj
+++ git-update.clj
@@ -1,17 +1,21 @@
-(require '[clojure.core.async :as async])
+(require '[clojure.java.io :as io])
 
 (defn exec [working-dir cmds]
-  (.. (new ProcessBuilder cmds) (directory (clojure.java.io/file working-dir)) (start) (waitFor)))
+  (.. (new ProcessBuilder cmds) (directory (io/file working-dir)) (start)))
 
-(->> "/Users/pgaultier/projects/ppro-1648791067/" 
-  (clojure.java.io/file)
-  (file-seq)
-  (filter #(.isDirectory %))
-  (filter #(= (.getName %) ".git"))
-  (map #(.getAbsolutePath (.getParentFile %)))
-  ; (map #(println % (str "'(cd " % " && git pull)'"))))
-  (map #(async/go [% (exec % ["git" "pull"])]))
-  (map #(async/<!! %)))
-  ; (print))
+(defn git-update [root]
+  (->> root 
+    (io/file)
+    (file-seq)
+    (filter #(.isDirectory %))
+    (filter #(= (.getName %) ".git"))
+    (map #(.getAbsolutePath (.getParentFile %)))
+    ; (map #(doto % prn))
+    (pmap (fn [path] 
+            (println path)
+            (let [p (exec path ["git" "pull"])]
+              (if (not= 0 (.waitFor p))
+                (println "NOT OK" path (.getErrorStream p))
+                (println "OK" path)))))))
 
-
+(git-update "/Users/pgaultier/projects/ppro-1648791067")
