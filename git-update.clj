7bf258f98b601b69de37d857b31ee34f2e5f113c
Philippe Gaultier <philippe.gaultier@ppro.com>
2022-04-04 17:01:05 +0200

    add channel for sync io

--- git-update.clj
+++ git-update.clj
@@ -1,21 +1,37 @@
 (require '[clojure.java.io :as io])
+(require '[clojure.core.async :as async])
 
 (defn exec [working-dir cmds]
   (.. (new ProcessBuilder cmds) (directory (io/file working-dir)) (start)))
 
-(defn git-update [root]
+(defn git-update [channel root]
   (->> root 
     (io/file)
     (file-seq)
     (filter #(.isDirectory %))
     (filter #(= (.getName %) ".git"))
     (map #(.getAbsolutePath (.getParentFile %)))
-    ; (map #(doto % prn))
     (pmap (fn [path] 
-            (println path)
+            (println "⏳ Fetching" path)
             (let [p (exec path ["git" "pull"])]
               (if (not= 0 (.waitFor p))
-                (println "NOT OK" path (.getErrorStream p))
-                (println "OK" path)))))))
+                (async/put! channel (str "❌ " path (slurp (.getErrorStream p))))
+                (async/put! channel (str "✓ " path))))))
+    (doall))
+  (async/close! channel))
 
-(git-update "/Users/pgaultier/projects/ppro-1648791067")
+(def channel (async/chan 100000))
+(prn "Start")
+(async/go (time (git-update channel "/Users/pgaultier/projects/ppro-1648791067")))
+(prn "End")
+
+(loop [x (async/<!! channel)]
+  (when x
+    (println x))
+  (recur (async/<!! channel)))
+(prn "End end")
+
+; (async/go-loop [x (async/<! channel)]
+;          (when x            ;; abort when channel is closed (nil? (<! c))
+;            (prn x)
+;            (recur (async/<! channel))))
